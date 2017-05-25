319a87504b06276cc3fcde4be1e648af106fe66a
Gwen <deathofmind@hotmail.fr>
2017-05-25 21:23:18 +0200

    Changement mineur
    Recherche par pseudo plutot que par id

--- api/controllers/memberController.js
+++ api/controllers/memberController.js
@@ -23,10 +23,10 @@ exports.create_a_member = function(req, res) {
 };
 
 exports.read_a_member = function(req, res) {
-    Member.findById(req.params.memberId, function(err, member) {
+    Member.find( {pseudo : req.params.memberId}, function(err, member) {
         if (err)
             res.send(err);
-        res.json(member);
+		res.json(member);
     });
 };
 
